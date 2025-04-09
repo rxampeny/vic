419b84e5324341917c90dc91299e5e5e17ec8ee8
rxampeny <77635261+rxampeny@users.noreply.github.com>
2025-04-09 10:05:44 +0200

    Delete config.js

--- config.js
+++
@@ -1,5 +0,0 @@
-
-// config.js
-module.exports = {
-  openaiApiKey: "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"
-};
